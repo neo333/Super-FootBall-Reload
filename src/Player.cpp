3c9def2c90e21aa0a46d1bb58b7f44b4f9c59c33
Neo333 <neo333_email@yahoo.it>
2012-03-14 17:21:43 +0100

    Nuovo sistema per il controllo del player tramite tastiera

--- src/Player.cpp
+++ src/Player.cpp
@@ -10,76 +10,31 @@ void Player::Run(const type_event::mess_event& mEvent){
 }
 
 void Player::Process_Moving(const type_event::mess_event& mEvent){
-	if(this->in_moving){
-		if(mEvent==type_event::KEY_RELASE){
-			switch(this->dir_moving){
-			case DIR_DOWN:
-				if(!sys_data::keys_main.DOWN_KEY){
-					this->in_moving=false;
-				}
-				break;
-			case DIR_UP:
-				if(!sys_data::keys_main.UP_KEY){
-					this->in_moving=false;
-				}
-				break;
-			case DIR_RIGHT:
-				if(!sys_data::keys_main.RIGHT_KEY){
-					this->in_moving=false;
-				}
-				break;
-			case DIR_LEFT:
-				if(!sys_data::keys_main.LEFT_KEY){
-					this->in_moving=false;
-				}
-				break;
-			}
-			if(!this->in_moving){
-				this->pSprite->Set_Speed(MyVector(0,0));
-			}
-		}
-	}
-	
-	if(mEvent==type_event::KEY_PRESS){
-		Sint16 x_vel=0, y_vel=0;
-		bool press_scan=false;
+	this->speed_player.Set_X(0);
+	this->speed_player.Set_Y(0);
 
-		switch(sys_data::keys_main.Get_Last_Key_operand()){
-		case SDLK_DOWN:
-			this->dir_moving=DIR_DOWN;
-			y_vel=this->rapid_move;
-			break;
-		case SDLK_UP:
-			this->dir_moving=DIR_UP;
-			y_vel=-this->rapid_move;
-			break;
-		case SDLK_LEFT:
-			this->dir_moving=DIR_LEFT;
-			x_vel=-this->rapid_move;
-			break;
-		case SDLK_RIGHT:
-			this->dir_moving=DIR_RIGHT;
-			x_vel=this->rapid_move;
-			break;
-		}
-		if(x_vel || y_vel){
-			this->in_moving=true;
-			this->pSprite->Set_Speed(MyVector(x_vel,y_vel));
-		}
+	if(sys_data::keys_main.DOWN_KEY){
+		this->speed_player.Set_Y(this->mod_speed);
 	}
+	if(sys_data::keys_main.UP_KEY){
+		this->speed_player.Set_Y(-this->mod_speed);
+	}
+	if(sys_data::keys_main.RIGHT_KEY){
+		this->speed_player.Set_X(this->mod_speed);
+	}
+	if(sys_data::keys_main.LEFT_KEY){
+		this->speed_player.Set_X(-this->mod_speed);
+	}
+
+	this->pSprite->Set_Speed(this->speed_player);
 }
 
 void Player::Process_SetFace(void){
-	switch(this->dir_moving){
-	case DIR_DOWN:
-		break;
-	case DIR_UP:
-		break;
-	case DIR_LEFT:
-		this->pSprite->Set_Face(this->dir_moving);
-		break;
-	case DIR_RIGHT:
-		this->pSprite->Set_Face(this->dir_moving);
-		break;
+	if(this->speed_player.Get_X() > 0){
+		this->pSprite->Set_Face(DIR_RIGHT);
 	}
+	if(this->speed_player.Get_X() < 0){
+		this->pSprite->Set_Face(DIR_LEFT);
+	}
+
 }
--- src/Player.h
+++ src/Player.h
@@ -8,17 +8,20 @@ class Player{
 private:
 	Sprite* pSprite;
 
-	bool in_moving;
-	Sint16 rapid_move;
-	Direction dir_moving;
+	MyVector speed_player;
+	Sint16 mod_speed;
 
 	void Process_Moving(const type_event::mess_event&);
 	void Process_Anim(void){
-		this->pSprite->Set_Anim(this->in_moving);
+		if(this->speed_player.Get_X() || this->speed_player.Get_Y()){
+			this->pSprite->Set_Anim(true);
+		}else{
+			this->pSprite->Set_Anim(false);
+		}
 	}
 	void Process_SetFace(void);
 public:
-	Player(void):pSprite(NULL),in_moving(false),rapid_move(2){
+	Player(void):pSprite(NULL),mod_speed(2){
 	}
 
 	/*Definisce su quale sprite il giocatore ha il controllo*/
